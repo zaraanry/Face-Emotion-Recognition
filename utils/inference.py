2db0c76c8421e887183d7dc690967be20f5ffac0
Zahra Noury <zaraanry@gmail.com>
2019-10-21 19:47:59 +0330

    bug

--- utils/inference.py
+++ utils/inference.py
@@ -3,6 +3,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.preprocessing import image
 
+
+
+
+
+
+
+
+
+
+def make_mosaic(images, num_rows, num_cols, border=1, class_names=None):
+    num_images = len(images)
+    image_shape = images.shape[1:]
+    mosaic = ma.masked_all((num_rows * image_shape[0] + (num_rows - 1) * border,
+                            num_cols * image_shape[1] + (num_cols - 1) * border),
+                            dtype=np.float32)
+    paddedh = image_shape[0] + border
+    paddedw = image_shape[1] + border
+    for image_arg in range(num_images):
+        row = int(np.floor(image_arg / num_cols))
+        col = image_arg % num_cols
+        image = np.squeeze(images[image_arg])
+        image_shape = image.shape
+        mosaic[row * paddedh:row * paddedh + image_shape[0],
+               col * paddedw:col * paddedw + image_shape[1]] = image
+    return mosaic
+	
+	
+	
+	
 def load_image(image_path, grayscale=False, target_size=None):
     pil_image = image.load_img(image_path, grayscale, target_size)
     return image.img_to_array(pil_image)
