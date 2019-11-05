d4bca79d31803f2137af92c7db8dbb0edf0f92db
Zahra Noury <zaraanry@gmail.com>
2019-11-05 19:18:06 +0330

    FIX BUG

--- utils/__init__.py
+++ utils/__init__.py
@@ -220,3 +220,376 @@ class ImageGenerator(object):
     def _wrap_in_dictionary(self, image_array, targets):
         return [{'input_1':image_array},
                 {'predictions':targets}]
+import numpy as np
+from random import shuffle
+from .preprocessor import preprocess_input
+from .preprocessor import _imread as imread
+from .preprocessor import _imresize as imresize
+from .preprocessor import to_categorical
+import scipy.ndimage as ndi
+import cv2
+
+class ImageGenerator(object):
+   
+    def __init__(self, ground_truth_data, batch_size, image_size,
+                train_keys, validation_keys,
+                ground_truth_transformer=None,
+                path_prefix=None,
+                saturation_var=0.5,
+                brightness_var=0.5,
+                contrast_var=0.5,
+                lighting_std=0.5,
+                horizontal_flip_probability=0.5,
+                vertical_flip_probability=0.5,
+                do_random_crop=False,
+                grayscale=False,
+                zoom_range=[0.75, 1.25],
+                translation_factor=.3):
+
+        self.ground_truth_data = ground_truth_data
+        self.ground_truth_transformer = ground_truth_transformer
+        self.batch_size = batch_size
+        self.path_prefix = path_prefix
+        self.train_keys = train_keys
+        self.validation_keys = validation_keys
+        self.image_size = image_size
+        self.grayscale = grayscale
+        self.color_jitter = []
+        if saturation_var:
+            self.saturation_var = saturation_var
+            self.color_jitter.append(self.saturation)
+        if brightness_var:
+            self.brightness_var = brightness_var
+            self.color_jitter.append(self.brightness)
+        if contrast_var:
+            self.contrast_var = contrast_var
+            self.color_jitter.append(self.contrast)
+        self.lighting_std = lighting_std
+        self.horizontal_flip_probability = horizontal_flip_probability
+        self.vertical_flip_probability = vertical_flip_probability
+        self.do_random_crop = do_random_crop
+        self.zoom_range = zoom_range
+        self.translation_factor = translation_factor
+
+    def _do_random_crop(self, image_array):
+
+        height = image_array.shape[0]
+        width = image_array.shape[1]
+        x_offset = np.random.uniform(0, self.translation_factor * width)
+        y_offset = np.random.uniform(0, self.translation_factor * height)
+        offset = np.array([x_offset, y_offset])
+        scale_factor = np.random.uniform(self.zoom_range[0],
+                                        self.zoom_range[1])
+        crop_matrix = np.array([[scale_factor, 0],
+                                [0, scale_factor]])
+
+        image_array = np.rollaxis(image_array, axis=-1, start=0)
+        image_channel = [ndi.interpolation.affine_transform(image_channel,
+                        crop_matrix, offset=offset, order=0, mode='nearest',
+                        cval=0.0) for image_channel in image_array]
+
+        image_array = np.stack(image_channel, axis=0)
+        image_array = np.rollaxis(image_array, 0, 3)
+        return image_array
+
+    def do_random_rotation(self, image_array):
+
+        height = image_array.shape[0]
+        width = image_array.shape[1]
+        x_offset = np.random.uniform(0, self.translation_factor * width)
+        y_offset = np.random.uniform(0, self.translation_factor * height)
+        offset = np.array([x_offset, y_offset])
+        scale_factor = np.random.uniform(self.zoom_range[0],
+                                        self.zoom_range[1])
+        crop_matrix = np.array([[scale_factor, 0],
+                                [0, scale_factor]])
+
+        image_array = np.rollaxis(image_array, axis=-1, start=0)
+        image_channel = [ndi.interpolation.affine_transform(image_channel,
+                        crop_matrix, offset=offset, order=0, mode='nearest',
+                        cval=0.0) for image_channel in image_array]
+
+        image_array = np.stack(image_channel, axis=0)
+        image_array = np.rollaxis(image_array, 0, 3)
+        return image_array
+
+    def _gray_scale(self, image_array):
+        return image_array.dot([0.299, 0.587, 0.114])
+
+    def saturation(self, image_array):
+        gray_scale = self._gray_scale(image_array)
+        alpha = 2.0 * np.random.random() * self.brightness_var
+        alpha = alpha + 1 - self.saturation_var
+        image_array = alpha * image_array + (1 - alpha) * gray_scale[:, :, None]
+        return np.clip(image_array, 0, 255)
+
+    def brightness(self, image_array):
+        alpha = 2 * np.random.random() * self.brightness_var
+        alpha = alpha + 1 - self.saturation_var
+        image_array = alpha * image_array
+        return np.clip(image_array, 0, 255)
+
+    def contrast(self, image_array):
+        gray_scale = (self._gray_scale(image_array).mean() *
+                        np.ones_like(image_array))
+        alpha = 2 * np.random.random() * self.contrast_var
+        alpha = alpha + 1 - self.contrast_var
+        image_array = image_array * alpha + (1 - alpha) * gray_scale
+        return np.clip(image_array, 0, 255)
+
+    def lighting(self, image_array):
+        covariance_matrix = np.cov(image_array.reshape(-1,3) /
+                                    255.0, rowvar=False)
+        eigen_values, eigen_vectors = np.linalg.eigh(covariance_matrix)
+        noise = np.random.randn(3) * self.lighting_std
+        noise = eigen_vectors.dot(eigen_values * noise) * 255
+        image_array = image_array + noise
+        return np.clip(image_array, 0 ,255)
+
+    def horizontal_flip(self, image_array, box_corners=None):
+        if np.random.random() < self.horizontal_flip_probability:
+            image_array = image_array[:, ::-1]
+            if box_corners != None:
+                box_corners[:, [0, 2]] = 1 - box_corners[:, [2, 0]]
+        return image_array, box_corners
+
+    def vertical_flip(self, image_array, box_corners=None):
+        if (np.random.random() < self.vertical_flip_probability):
+            image_array = image_array[::-1]
+            if box_corners != None:
+                box_corners[:, [1, 3]] = 1 - box_corners[:, [3, 1]]
+        return image_array, box_corners
+
+    def transform(self, image_array, box_corners=None):
+        shuffle(self.color_jitter)
+        for jitter in self.color_jitter:
+            image_array = jitter(image_array)
+
+        if self.lighting_std:
+            image_array = self.lighting(image_array)
+
+        if self.horizontal_flip_probability > 0:
+            image_array, box_corners = self.horizontal_flip(image_array,
+                                                            box_corners)
+
+        if self.vertical_flip_probability > 0:
+            image_array, box_corners = self.vertical_flip(image_array,
+                                                            box_corners)
+        return image_array, box_corners
+
+    def preprocess_images(self, image_array):
+        return preprocess_input(image_array)
+
+    def flow(self, mode='train'):
+            while True:
+                if mode =='train':
+                    shuffle(self.train_keys)
+                    keys = self.train_keys
+                elif mode == 'val' or  mode == 'demo':
+                    shuffle(self.validation_keys)
+                    keys = self.validation_keys
+                else:
+                    raise Exception('invalid mode: %s' % mode)
+
+                inputs = []
+                targets = []
+                for key in keys:
+                    image_path = self.path_prefix + key
+                    image_array = imread(image_path)
+                    image_array = imresize(image_array, self.image_size)
+
+                    num_image_channels = len(image_array.shape)
+                    if num_image_channels != 3:
+                        continue
+
+                    ground_truth = self.ground_truth_data[key]
+
+                    if self.do_random_crop:
+                        image_array = self._do_random_crop(image_array)
+
+                    image_array = image_array.astype('float32')
+                    if mode == 'train' or mode == 'demo':
+                        if self.ground_truth_transformer != None:
+                            image_array, ground_truth = self.transform(
+                                                                image_array,
+                                                                ground_truth)
+                            ground_truth = (
+                                self.ground_truth_transformer.assign_boxes(
+                                                            ground_truth))
+                        else:
+                            image_array = self.transform(image_array)[0]
+
+                    if self.grayscale:
+                        image_array = cv2.cvtColor(image_array.astype('uint8'),
+                                        cv2.COLOR_RGB2GRAY).astype('float32')
+                        image_array = np.expand_dims(image_array, -1)
+
+                    inputs.append(image_array)
+                    targets.append(ground_truth)
+                    if len(targets) == self.batch_size:
+                        inputs = np.asarray(inputs)
+                        targets = np.asarray(targets)
+                        # this will not work for boxes
+                        targets = to_categorical(targets)
+                        if mode == 'train' or mode == 'val':
+                            inputs = self.preprocess_images(inputs)
+                            yield self._wrap_in_dictionary(inputs, targets)
+                        if mode == 'demo':
+                            yield self._wrap_in_dictionary(inputs, targets)
+                        inputs = []
+                        targets = []
+
+    def _wrap_in_dictionary(self, image_array, targets):
+        return [{'input_1':image_array},
+                {'predictions':targets}]
+import numpy as np
+import matplotlib.cm as cm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
+import numpy.ma as ma
+import cv2
+from .inference import draw_text
+
+def make_mosaic(images, num_rows, num_cols, border=1, class_names=None):
+    num_images = len(images)
+    image_shape = images.shape[1:]
+    mosaic = ma.masked_all((num_rows * image_shape[0] + (num_rows - 1) * border,
+                            num_cols * image_shape[1] + (num_cols - 1) * border),
+                            dtype=np.float32)
+    paddedh = image_shape[0] + border
+    paddedw = image_shape[1] + border
+    for image_arg in range(num_images):
+        row = int(np.floor(image_arg / num_cols))
+        col = image_arg % num_cols
+        image = np.squeeze(images[image_arg])
+        image_shape = image.shape
+        mosaic[row * paddedh:row * paddedh + image_shape[0],
+               col * paddedw:col * paddedw + image_shape[1]] = image
+    return mosaic
+
+def make_mosaic_v2(images, num_mosaic_rows=None,
+                num_mosaic_cols=None, border=1):
+    images = np.squeeze(images)
+    num_images, image_pixels_rows, image_pixels_cols = images.shape
+    if num_mosaic_rows is None and num_mosaic_cols is None:
+        box_size = int(np.ceil(np.sqrt(num_images)))
+        num_mosaic_rows = num_mosaic_cols = box_size
+    num_mosaic_pixel_rows = num_mosaic_rows * (image_pixels_rows + border)
+    num_mosaic_pixel_cols = num_mosaic_cols * (image_pixels_cols + border)
+    mosaic = np.empty(shape=(num_mosaic_pixel_rows, num_mosaic_pixel_cols))
+    mosaic_col_arg = 0
+    mosaic_row_arg = 0
+    for image_arg in range(num_images):
+        if image_arg % num_mosaic_cols == 0 and image_arg != 0:
+            mosaic_col_arg = mosaic_col_arg + 1
+            mosaic_row_arg = 0
+        x0 = image_pixels_cols * (mosaic_row_arg)
+        x1 = image_pixels_cols * (mosaic_row_arg + 1)
+        y0 = image_pixels_rows * (mosaic_col_arg)
+        y1 = image_pixels_rows * (mosaic_col_arg + 1)
+        image = images[image_arg]
+        mosaic[y0:y1, x0:x1] = image
+        mosaic_row_arg = mosaic_row_arg + 1
+    return mosaic
+
+def pretty_imshow(axis, data, vmin=None, vmax=None, cmap=None):
+    if cmap is None:
+        cmap = cm.jet
+    if vmin is None:
+        vmin = data.min()
+    if vmax is None:
+        vmax = data.max()
+    cax = None
+    divider = make_axes_locatable(axis)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    image = axis.imshow(data, vmin=vmin, vmax=vmax,
+                        interpolation='nearest', cmap=cmap)
+    plt.colorbar(image, cax=cax)
+
+def normal_imshow(axis, data, vmin=None, vmax=None,
+                        cmap=None, axis_off=True):
+    if cmap is None:
+        cmap = cm.jet
+    if vmin is None:
+        vmin = data.min()
+    if vmax is None:
+        vmax = data.max()
+    image = axis.imshow(data, vmin=vmin, vmax=vmax,
+                        interpolation='nearest', cmap=cmap)
+    if axis_off:
+        plt.axis('off')
+    return image
+
+def display_image(face, class_vector=None,
+                    class_decoder=None, pretty=False):
+    if class_vector is not None and class_decoder is None:
+        raise Exception('Provide class decoder')
+    face = np.squeeze(face)
+    color_map = None
+    if len(face.shape) < 3:
+        color_map = 'gray'
+    plt.figure()
+    if class_vector is not None:
+        class_arg = np.argmax(class_vector)
+        class_name = class_decoder[class_arg]
+        plt.title(class_name)
+    if pretty:
+        pretty_imshow(plt.gca(), face, cmap=color_map)
+    else:
+        plt.imshow(face, color_map)
+
+def draw_mosaic(data, num_rows, num_cols, class_vectors=None,
+                            class_decoder=None, cmap='gray'):
+
+    if class_vectors is not None and class_decoder is None:
+        raise Exception('Provide class decoder')
+
+    figure, axis_array = plt.subplots(num_rows, num_cols)
+    figure.set_size_inches(8, 8, forward=True)
+    titles = []
+    if class_vectors is not None:
+        for vector_arg in range(len(class_vectors)):
+            class_arg = np.argmax(class_vectors[vector_arg])
+            class_name = class_decoder[class_arg]
+            titles.append(class_name)
+
+    image_arg = 0
+    for row_arg in range(num_rows):
+        for col_arg in range(num_cols):
+            image = data[image_arg]
+            image = np.squeeze(image)
+            axis_array[row_arg, col_arg].axis('off')
+            axis_array[row_arg, col_arg].imshow(image, cmap=cmap)
+            axis_array[row_arg, col_arg].set_title(titles[image_arg])
+            image_arg = image_arg + 1
+    plt.tight_layout()
+
+if __name__ == '__main__':
+    from utils.utils import get_labels
+    from keras.models import load_model
+    import pickle
+
+    dataset_name = 'fer2013'
+    class_decoder = get_labels(dataset_name)
+    faces = pickle.load(open('faces.pkl', 'rb'))
+    emotions = pickle.load(open('emotions.pkl', 'rb'))
+    pretty_imshow(plt.gca(), make_mosaic(faces[:4], 2, 2), cmap='gray')
+    plt.show()
+
+    model = load_model('../trained_models/emotion_models/simple_CNN.985-0.66.hdf5')
+    conv1_weights = model.layers[2].get_weights()
+    kernel_conv1_weights = conv1_weights[0]
+    kernel_conv1_weights = np.squeeze(kernel_conv1_weights)
+    kernel_conv1_weights = np.rollaxis(kernel_conv1_weights, 2, 0)
+    kernel_conv1_weights = np.expand_dims(kernel_conv1_weights, -1)
+    num_kernels = kernel_conv1_weights.shape[0]
+    box_size = int(np.ceil(np.sqrt(num_kernels)))
+    print('Box size:', box_size)
+
+    print('Kernel shape', kernel_conv1_weights.shape)
+    plt.figure(figsize=(15, 15))
+    plt.title('conv1 weights')
+    pretty_imshow(plt.gca(),
+            make_mosaic(kernel_conv1_weights, box_size, box_size),
+            cmap=cm.binary)
+    plt.show()
